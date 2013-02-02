ad27dd1c36217a68418063d7a015a0d261b81241
Jameson Proctor <jameson@jp1971.com>
2013-02-01 23:01:43 -0500

    Removing exit class.  Adding PWM code.  Stubbing out control logic.

--- botpi.py
+++ botpi.py
@@ -6,9 +6,28 @@ import sys
 
 #Servo and PWM driver includes
 #from Adafruit_PWM_Servo_Driver import PWM
-#import time
+#import time\
 
-ASSETS_DIR = os.path.join(os.path.abspath("."), u"assets")
+
+# Initialise the PWM device using the default address
+pwm = PWM(0x40, debug=True)
+
+servoMin = 150  # Min pulse length out of 4096
+servoMax = 600  # Max pulse length out of 4096
+
+def setServoPulse(channel, pulse):
+  pulseLength = 1000000                   # 1,000,000 us per second
+  pulseLength /= 60                       # 60 Hz
+  print '%d us per period' % pulseLength
+  pulseLength /= 4096                     # 12 bits of resolution
+  print '%d us per bit' % pulseLength
+  pulse *= 1000
+  pulse /= pulseLength
+  pwm.setPWM(channel, 0, pulse)
+
+pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
+
+ASSETS_DIR = os.path.join(os.path.abspath('.'), u'assets')
 
 class AjaxApp(object):
     def index(self):
@@ -16,14 +35,24 @@ class AjaxApp(object):
     index.exposed = True
 
     def command(self, command):
+        if command == 'fwd'
+            pwm.setPWM(,,)
+
+        else if command == 'lft'
+            pwm.setPWM(,,)     
+
+        else if command == 'stp'
+            pwm.setPWM(,,)  
+
+        else if command == 'rgt'
+            pwm.setPWM(,,)  
+
+        else if command == 'rev'
+            pwm.setPWM(,,)                                         
+
         cherrypy.response.headers['Content-Type'] = 'application/json'
         return simplejson.dumps(dict(command="%s" % command))
-    command.exposed = True        
-
-    def exit(self, name):
-        cherrypy.engine.exit()
-    index.exposed = True        
-
+    command.exposed = True             
 
 config = {'/assets':
                 {'tools.staticdir.on': True,
